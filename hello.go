edfd7572e77e5c5963ccd5a78cf424f018f65390
Jimena Barseghian <aghassi.lucas@gmail.com>
2023-02-11 15:26:35 -0300

    Added Hellos func

--- hello.go
+++ hello.go
@@ -16,7 +16,6 @@ func main() {
 	log.SetFlags(0)
 
 	// Request a greeting message.
-
 	message, err := greetings.Hello("Gladys")
 	// If an error was returned, print it to the console and
 	// exit the program.
@@ -27,4 +26,17 @@ func main() {
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
+
+	fmt.Println("========================================================")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
